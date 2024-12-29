9266655e21831de430a76fd139ca9cd637ae88a5
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-12-29 18:37:33 +0600

    creating academicFaculty

---
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
+
+export const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique:true
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const academicFacultyModel = model<TAcademicFaculty>('academicFaculty',academicFacultySchema)
