446f7789fcfdd575f7c5dd3746d2bc39e6812e14
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2024-12-29 18:24:16 +0600

    creating academicFaculty

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -13,12 +13,25 @@ export const findLastStudentId = async () => {
     .sort({
       createdAt: -1,
     });
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generatedStudentId = async (payload) => {
   // first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId =  (0).toString(); 
+
+  const lastStudentId = await findLastStudentId()
+  
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4,6) //01
+  const lastStudentYear = lastStudentId?.substring(0,4) //2030
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+
+  if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentSemesterYear){
+    currentId = lastStudentId.substring(6); //0001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
